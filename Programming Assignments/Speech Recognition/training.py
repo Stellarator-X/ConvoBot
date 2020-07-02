e7e1b30212c7d0bef8df7100da75dd81382c31b2
stellarator-x <admathur@iitk.ac.in>
2020-07-02 06:42:19 +0530

    training pipeline

--- Programming Assignments/Speech Recognition/training.py
+++ Programming Assignments/Speech Recognition/training.py
@@ -2,7 +2,19 @@ import argparse
 
 # Adding arguments
 parser = argparse.ArgumentParser()
-parser.add_argument()
+parser.add_argument("--dataset_path", default = 'bin/LibriSpeech', help = "Path to the dataset")
+parser.add_argument("--checkpoint_path", default = 'bin/Training/cp.ckpt', help = "Path to the dataset")
+parser.add_argument("-augment", default = False, help='whether to add augmentations', action = 'store_true')
+parser.add_argument("-e", "--epochs", type=int, default = 10, help='Number of epochs')
+parser.add_argument("-bs" "--batch_size", type  = int, default = 50, help = "batch size")
+
+args = parser.parse_args()
+
+DATASET_PATH = args.dataset_path
+CKPT_PATH = args.checkpoint_path
+AUGMENT = args.augment
+EPOCHS = args.epochs
+BATCH_SIZE = args.batch_size
 
 # Dependencies
 from ds_utils.augmentation import *
@@ -13,7 +25,7 @@ import os
 
 # Defining checkpoint variables
 import os
-checkpoint_path = "bin/Training/cp.ckpt"
+checkpoint_path = CKPT_PATH
 checkpoint_dir = os.path.dirname(checkpoint_path)
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1, period = 5)
 
@@ -33,7 +45,7 @@ def plothist(history):
     plt.show()
 
 # Dataset
-datset = get_data(verbose=True)
+datset = get_data(path = DATASET_PATH, verbose=True)
 files = datset[:, 0]
 file_to_label = { datset[i, 0]:datset[i,1] for i in range(datset.shape[0])}
 
@@ -46,8 +58,8 @@ try:
 except:
     print("No previous checkpoints")
 
-speechgen = SpeechDataGenerator(files, file_to_label)
+speechgen = SpeechDataGenerator(files, file_to_label, augment = AUGMENT, batch_size=BATCH_SIZE)
 
-hist = model.fit(speechgen, epochs = 20)
+hist = model.fit(speechgen, epochs = EPOCHS)
 
 plothist(hist)
